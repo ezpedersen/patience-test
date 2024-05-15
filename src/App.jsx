9cba09c12f3200ea2df4f381f6f88cfb79c53799
Erik Pedersen <59267386+ezpedersen@users.noreply.github.com>
2024-05-15 09:31:27 -0700

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -19,38 +16,18 @@ function App() {
     storageBucket: "patience-test-cf25c.appspot.com",
     messagingSenderId: "471248396275",
     appId: "1:471248396275:web:51a9134f70f870e4937c86",
-    measurementId: "G-PT7LTB2WPK"
+    measurementId: "G-PT7LTB2WPK",
   };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello WOrld!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
