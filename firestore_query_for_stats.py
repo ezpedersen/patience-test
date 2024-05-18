4d190a47971b2ad30a4aa3116aaec3a81aa11a3e
Kelvin <63611619+kelvinjou@users.noreply.github.com>
2024-05-18 16:13:22 -0700

    Add files via upload

---
+++ firestore_query_for_stats.py
@@ -0,0 +1,48 @@
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+from google.cloud.firestore_v1.base_query import FieldFilter
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as stats
+
+cred = credentials.Certificate("/Users/kelvinj/Downloads/irvington-opinions-firebase-adminsdk-yk8ai-eb4ffabf81.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+row_list = []
+row_of_gpa = []
+
+cities_ref = db.collection("opinions")
+docs = (
+    db.collection("opinions")
+    # .where(filter=FieldFilter("gpa", "==", 3.72))
+    .stream()
+)
+
+
+for doc in docs:
+    dict = doc.to_dict()
+    print(f"{doc.id} => {dict['gpa']} => {dict['numAnswered']}")
+
+    new_dict = {'UUID': doc.id, 'gpa' : dict['gpa'], 'responses': dict['numAnswered']}
+    row_list.append(new_dict)
+    # print(f"{doc.id} => {doc.to_dict()}")
+
+df = pd.DataFrame(row_list)
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+line = slope * dict['gpa'] + intercept
+
+df.columns = ['UUID', 'gpa', 'responses']
+df.to_csv('/Users/kelvinj/Downloads/irvington-high-school-opinions-results.csv')
+
+plt.scatter(x=df['gpa'], y=df['responses'])
+plt.show()
+
+
+
+
+
